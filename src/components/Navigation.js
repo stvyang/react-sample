450b6f9996bab1b3bb416fbf03bd9d3f2d02f025
stvyang <stv.yang10@gmail.com>
2018-04-24 01:51:57 +0700

    Add navigation on UI

---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>Bookpedia</li>
+        <li>Book</li>
+        <li>Customer</li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navigation;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'; // Library to create React Component
+import ReactDOM from 'react-dom'; // Library to place Component to DOM
 import App from './App';
+import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App txt="prop" />, document.getElementById('root'));
 registerServiceWorker();
