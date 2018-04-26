74f6c127bb3533ee5086247310a1d04d9df16162
stvyang <stv.yang10@gmail.com>
2018-04-26 23:53:11 +0700

    Refactor booklist component on admin page

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -1,38 +1,11 @@
-import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
-import { getBooks } from '../../queries';
-import Loading from '../global/Loading';
+import React from 'react';
+import BookList from './BookList';
 
-class Admin extends Component {
-  displayBooks() {
-    let data = this.props.data;
-    if (data.loading) {
-      return <Loading />;
-    }
-    else {
-      if (data.books.length > 0) {
-        return data.books.map(book => (
-            <li key={book.id}>{book.author}</li>
-          )
-        )
-      } else {
-        return (
-          <div>Unfortunately, you didn't have any book yet.</div>
-        )
-      }
-    }
-  }
+const Admin = () => (
+  <div>
+    <h3>Your books...</h3>
+    <BookList />
+  </div>
+)
 
-  render() {
-    return (
-      <div>
-        <h3>Your books...</h3>
-        <div id="book-list">
-          {this.displayBooks()}
-        </div>
-      </div>
-    )
-  }
-}
-
-export default graphql(getBooks)(Admin);
+export default Admin;
---
+++ src/components/global/NotFound.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const NotFound = (props) => (
+  <div>Unfortunately, you didn't have any {props.name} yet.</div>
+);
+
+export default NotFound;
