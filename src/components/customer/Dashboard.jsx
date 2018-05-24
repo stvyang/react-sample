376819a69d6b76322997fa0f68def3b1a51c13f3
stvyang <stv.yang10@gmail.com>
2018-05-24 21:22:11 +0700

    Edit dashboard to support View Order functionality

--- src/components/customer/Dashboard.jsx
+++ src/components/customer/Dashboard.jsx
@@ -1,20 +1,67 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { graphql } from 'react-apollo';
 import { Link } from 'react-router-dom';
+import { getOrders } from '../../queries';
+import Loading from '../global/Loading';
+import NotFound from '../global/NotFound';
 
-class Dashboard extends Component {
-  render() {
-    const { match } = this.props;
+function displayOrders(orders, match) {
+  const displayedOrders = orders.map(order => {
     return (
-      <div>
-        <h3>Orders</h3>
-        <button>
-          <Link className="a-primary" to={`${match.url}/order/create`}>
-            Place Order
-          </Link>
-        </button>
+      <div className="order-card">
+        <div className="title">
+          <Link to={`${match.url}/order/${order.id}`}>{order.id}</Link>
+        </div>
+        <div id="order-date">{order.orderDate}</div>
+        <div id="order-total">${order.totalPayment}</div>
       </div>
     );
+  });
+  return displayedOrders;
+}
+
+function displayDashboard(isLoading, orders, match) {
+  if (isLoading) {
+    return <Loading />;
+  } else if (orders.length > 0) {
+    return (
+      <div className="order-board">
+        <div className="card-list">{displayOrders(orders, match)}</div>
+      </div>
+    );
+  } else {
+    return <NotFound name="Unfortunately, you did not have any order yet." />;
   }
 }
 
-export default Dashboard;
+function Dashboard(props) {
+  const { data, match } = props;
+  return (
+    <div>
+      <h3>Orders</h3>
+      <button>
+        <Link className="a-primary" to={`${match.url}/order/create`}>
+          Place Order
+        </Link>
+      </button>
+      {displayDashboard(data.loading, data.orders, match)}
+    </div>
+  );
+}
+
+Dashboard.propTypes = {
+  data: PropTypes.arrayOf(PropTypes.shape({
+    orderDate: PropTypes.instanceOf(Date),
+    totalPayment: PropTypes.number,
+  })).isRequired,
+  match: PropTypes.shape({
+    url: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default graphql(getOrders, {
+  options: {
+    fetchPolicy: 'network-only',
+  },
+})(Dashboard);
